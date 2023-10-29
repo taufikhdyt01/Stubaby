56baccab7465ebffab2883737e3ba0c0389e91f8
Delisya Jayadi <delisyajayadi10@gmail.com>
2023-10-30 00:29:57 +0700

    update kecil

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -9,11 +9,41 @@
 
 <body>
     <header>
-        
+
     </header>
 
     <section>
+        <!-- 1 -->
+        <div>
 
+        </div>
+        <!-- 2 -->
+        <div>
+            <div>
+
+            </div>
+            <div>
+                <h2>Apa itu</h2>
+                <h1>StuBaby</h1>
+                <h2>?</h2>
+                <p>
+                    StuBaby sendiri merupakan website yang menyediakan layanan untuk ibu dan anak dengan tujuan untuk melakukan pencegahan stunting khususnya untuk anak. 
+                    Layanan yang diberikan untuk pencegahan ini berupa dengan adanya fitur merekam jejak perkembangan dan pertumbuhan anak secara digital serta mudah dan juga dapat melakukan konsultasi secara langsung dengan ahli gizi dalam tujuan untuk menumbuhkan anak dengan giat dan mencegah dari stunting.
+                </p>
+            </div>
+        </div>
+        <!-- 3 -->
+        <div>
+
+        </div>
+        <!-- 4 -->
+        <div>
+
+        </div>
+        <!-- 5 -->
+        <div>
+
+        </div>
     </section>
 
     <footer>
