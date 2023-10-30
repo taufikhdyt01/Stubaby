1e8854cac49d39374c4096ab562948d7452d78ae
Delisya Jayadi <delisyajayadi10@gmail.com>
2023-10-30 21:28:53 +0700

    update login dan register
    update login register, ada juga update di tailwind config js untuk custom dan update di folder rescource/img

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -48,7 +48,7 @@
                             </div>
                             <button type="submit" class="w-full text-white bg-primary hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary font-medium rounded-lg text-sm px-5 py-2.5 text-center ">Daftar akun baru</button>
                             <p class="text-sm font-light text-gray-500 text-center">
-                                Sudah punya akun? <a href="#" class="font-medium text-primary hover:underline ">Masuk akun</a>
+                                Sudah punya akun? <a href="#" class="font-medium text-primary hover:underline">Masuk akun</a>
                             </p>
                         </form>
                     </div>
