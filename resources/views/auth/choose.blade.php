7948eb30619d6b383d523e1bb7ebbd24e4b11f63
Delisya Jayadi <delisyajayadi10@gmail.com>
2023-11-06 21:19:53 +0700

    update choose option

--- resources/views/auth/choose.blade.php
+++ resources/views/auth/choose.blade.php
@@ -8,13 +8,34 @@
     <title>StuBaby</title>
 </head>
 
-<body>
-    <img src="{{ asset('/img/logo_2.svg') }}" alt="">
-    <div>
-        <h2>
+<body class="">
+    <img src="{{ asset('/img/logo_2.svg') }}" class="mx-auto" alt="">
+    <div class="text-center align-center justify-center">
+        <h2 class="text-2xl font-semibold">
             Anda mendaftar sebagai apa?
         </h2>
-        
+        <div class="flex align-center justify-center space-x-10">
+            <!-- 1 -->
+            <a href="#">
+                <div class="bg-gradient-to-t from-indigo-400 to-indigo-300 rounded-2xl w-[350px] py-8 px-16 drop-shadow-md hover:from-indigo-500 hover:to-indigo-400 ">
+                    <img class="w-auto h-auto" src="{{ asset('/img/img_chooseirt.svg') }}" alt="">
+                    <h5 class="text-[24px] font-bold mt-6 drop-shadow-md text-white">
+                        Ibu Rumah Tangga
+                    </h5>
+                </div>
+            </a>
+
+            <!-- 2 -->
+            <a href="#">
+                <div class="bg-gradient-to-t from-emerald-400 to-green-300 rounded-2xl w-[350px] py-8 px-16 drop-shadow-md hover:from-emerald-500 hover:to-green-500 ">
+                    <img class="w-auto h-auto" src="{{ asset('/img/img_chooseahligizi.svg') }}" alt="">
+                    <h5 class="text-[24px] font-bold mt-6 drop-shadow-md text-white">
+                        Ahli Gizi
+                    </h5>
+                </div>
+            </a>
+        </div>
+
     </div>
 </body>
 
